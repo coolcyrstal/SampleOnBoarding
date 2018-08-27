64a49dd4ddeca8b06365f379d65fbe9137712fdf
coolcyrstal <chayenjr.sk131@gmail.com>
2018-08-27 16:46:48 +0700

    add fragment

---
+++ app/src/main/java/com/example/chayent/sampleonboarding/PagerFragment.java
@@ -0,0 +1,53 @@
+package com.example.chayent.sampleonboarding;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PagerFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public PagerFragment() {
+        // Required empty public constructor
+    }
+
+    public static PagerFragment newInstance() {
+        PagerFragment fragment = new PagerFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_pager, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
