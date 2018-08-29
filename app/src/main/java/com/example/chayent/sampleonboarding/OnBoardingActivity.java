ed85046cca16d9fafcc4108f5f7d38b67042488e
coolcyrstal <chayenjr.sk131@gmail.com>
2018-08-29 15:16:43 +0700

    change UI flow

--- app/src/main/java/com/example/chayent/sampleonboarding/OnBoardingActivity.java
+++ app/src/main/java/com/example/chayent/sampleonboarding/OnBoardingActivity.java
@@ -5,9 +5,9 @@ import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -27,15 +27,13 @@ public class OnBoardingActivity extends AppCompatActivity {
     @BindView(R.id.onboarding_image)
     ImageView onBoardingImage;
 
-    private int mLinearLayoutHeight;
-    private int mViewPagerTotalPage;
-    private int mViewPagerHeight;
     private int mPreviousPosition = 0;
-    private Animation animation_slide_up;
-    private Animation animation_slide_down;
-    private Animation animation_fade_out;
-    private LinearLayout mLinearLayout;
-    private int[] imageBackground = new int[]{
+    private Animation mAnimationSlideUp;
+    private Animation mAnimationSlideDown;
+    private Animation mAnimationFadeOut;
+    private AnimationSet mAnimationSet;
+
+    private int[] mImageBackground = new int[]{
             R.drawable.onboarding_image_1,
             R.drawable.onboarding_image_2,
             R.drawable.onboarding_image_3
@@ -46,56 +44,56 @@ public class OnBoardingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
         ButterKnife.bind(this);
-
-        setAnimation();
-        createOnBoardingViewPager();
-        setCirclePageIndicator();
-        setViewPagerListener();
+        setUpView();
     }
 
-    private void createOnBoardingViewPager() {
+    private void setUpView() {
+        mAnimationSet = new AnimationSet(false);
+        mAnimationSlideUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+        mAnimationSlideDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);
+        mAnimationFadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPagerAdapter.addFrag(new PagerFragment(), "page_1");
         viewPagerAdapter.addFrag(new PagerFragment(), "page_2");
         viewPagerAdapter.addFrag(new PagerFragment(), "page_3");
         viewPager.setAdapter(viewPagerAdapter);
-        mViewPagerTotalPage = viewPager.getAdapter().getCount();
-    }
 
-    private void setCirclePageIndicator() {
         circlePageIndicator.setViewPager(viewPager);
-    }
 
-    private void setViewPagerListener() {
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//                onBoardingImage.setAlpha(1 - positionOffset);
-//                Log.d("checkposition", "" + position);
-//                onBoardingImage.startAnimation(animation_fade_out);
+
             }
 
             @Override
             public void onPageSelected(int position) {
-                startImageAnimation(position);
+                if (position != mPreviousPosition) {
+                    startImageAnimation(position);
+                    mPreviousPosition = position;
+                }
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
             }
         });
+
     }
 
     public void startImageAnimation(final int position) {
-        startExpandViewAnimation(position);
-        animation_slide_up.setAnimationListener(new Animation.AnimationListener() {
+        mAnimationSet.addAnimation(mAnimationSlideUp);
+        mAnimationSet.addAnimation(mAnimationFadeOut);
+        mAnimationSet.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
+
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                onBoardingImage.setImageResource(imageBackground[position]);
+                onBoardingImage.setImageResource(mImageBackground[position]);
             }
 
             @Override
@@ -103,69 +101,57 @@ public class OnBoardingActivity extends AppCompatActivity {
 
             }
         });
-        onBoardingImage.startAnimation(animation_slide_up);
+        onBoardingImage.startAnimation(mAnimationSet);
     }
 
-    private void startExpandViewAnimation(int position) {
-        mViewPagerHeight = viewPager.getMeasuredHeight();
-        mLinearLayoutHeight = viewPager.getChildAt(0).findViewById(R.id.onboarding_linear_layout).getMeasuredHeight();
-        mLinearLayout = viewPager.getChildAt(position).findViewById(R.id.onboarding_linear_layout);
-        if(position != mPreviousPosition){
-            setViewHeight(mLinearLayout, mViewPagerHeight);
-            mPreviousPosition = position;
-        }
-    }
-
-    private void setViewHeight(final View view, final int viewHeight) {
-        ValueAnimator anim = ValueAnimator.ofInt(mLinearLayoutHeight, viewHeight);
-        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                int val = (Integer) valueAnimator.getAnimatedValue();
-                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-                layoutParams.height = val;
-                view.setLayoutParams(layoutParams);
-            }
-        });
-        anim.setDuration(500);
-        anim.start();
-        anim.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                ValueAnimator animEnd = ValueAnimator.ofInt(viewHeight, mLinearLayoutHeight);
-                animEnd.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                        int val = (Integer) valueAnimator.getAnimatedValue();
-                        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-                        layoutParams.height = val;
-                        view.setLayoutParams(layoutParams);
-                    }
-                });
-                animEnd.setDuration(300);
-                animEnd.start();
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                animation.cancel();
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-    }
-
-    private void setAnimation() {
-        animation_slide_down = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);
-        animation_slide_up = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
-        animation_fade_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
-    }
+//    private void startAnimationExpand(int position) {
+//        final int viewPagerHeight = viewPager.getMeasuredHeight();
+//        final int linearLayoutHeight = viewPager.getChildAt(0).findViewById(R.id.onboarding_linear_layout).getMeasuredHeight();
+//        final LinearLayout linearLayout = viewPager.getChildAt(position).findViewById(R.id.onboarding_linear_layout);
+//
+//        ValueAnimator anim = ValueAnimator.ofInt(linearLayoutHeight, viewPagerHeight);
+//        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+//                int val = (Integer) valueAnimator.getAnimatedValue();
+//                ViewGroup.LayoutParams layoutParams = linearLayout.getLayoutParams();
+//                layoutParams.height = val;
+//                linearLayout.setLayoutParams(layoutParams);
+//            }
+//        });
+//        anim.setDuration(500).start();
+//        anim.addListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                ValueAnimator animEnd = ValueAnimator.ofInt(viewPagerHeight, linearLayoutHeight);
+//                animEnd.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//                    @Override
+//                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+//                        int val = (Integer) valueAnimator.getAnimatedValue();
+//                        ViewGroup.LayoutParams layoutParams = linearLayout.getLayoutParams();
+//                        layoutParams.height = val;
+//                        linearLayout.setLayoutParams(layoutParams);
+//                    }
+//                });
+//                animEnd.setDuration(300);
+//                animEnd.start();
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//                animation.cancel();
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
+//
+//    }
 }
